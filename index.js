5793427c19ca9aa3c12d876c48059f9ec02872c7
Clay Smith <smithclay@gmail.com>
2017-12-15 10:48:57 -0800

    fix typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./app');
 const server = awsServerlessExpress.createServer(app);
 
-const AWSXray = require('aws-xray-sdk');
+const AWSXRay = require('aws-xray-sdk');
 const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
